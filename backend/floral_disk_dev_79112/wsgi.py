b6af07d9bd1131c46797826367f9a2531e8faa98
Crowdbotics <engineering@crowdbotics.com>
2023-04-26 06:01:42 +0000

    Initial commit.

---
+++ backend/floral_disk_dev_79112/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for floral_disk_dev_79112 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'floral_disk_dev_79112.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "floral-disk-dev-79112",
+  url: "https://floral-disk-dev-79112.botics.co",
+  api: "https://floral-disk-dev-79112.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
